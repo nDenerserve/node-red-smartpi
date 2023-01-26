e9d65bdb0a6dee34a11380fb18704f4b8414ac91
Jens Ramhorst <jens@ramhorst.eu>
2023-01-26 18:58:00 +0100

    2022-01-26

--- smartpi-modules-digital-out.js
+++ smartpi-modules-digital-out.js
@@ -1,44 +1,54 @@
-module.exports = function(RED) {
-    function SmartPiEtemperatureValues(config) {
+module.exports = function (RED) {
+    const got = require('got');
+
+
+    function SmartPiDigitalOut(config) {
+
         RED.nodes.createNode(this, config);
-
-        this.indicator = config.indicator;
         var node = this;
+        this.indicator = config.indicator;
+        this.server = config.server;
+        this.token = config.token;
+        this.bits = config.bits;
+        this.output = config.output;
 
-        fs = require('fs');
-        watch = require('node-watch');
-        watch('/var/tmp/smartpi/etemperature_values', { recursive: true }, function(evt, name) {
-            fs.readFile('/var/tmp/smartpi/etemperature_values', 'utf8', function(err, data) {
-                if (err) {
-                    return console.log(err);
+        node.on('input', function (msg) {
+
+            console.log("Config: ");
+            console.log("Server: " + this.server);
+            console.log("Token: " + this.token);
+            console.log("Bits length: " + this.bits.length);
+            console.log("Bits: " + this.bits);
+            console.log("Output: " + this.output);
+
+            var request = this.server + "/api/v1/module/digitalout/";
+
+            for (let element of this.bits) {
+                console.log(element);
+                if (element == false) {
+                    request = request + "0"
+                } else if (element == true) {
+                    request = request + "1"
                 }
-                var values = data.split(';');
-                var output = {
-                    "date": values[0].replace(/['"]+/g, ''),
-                    "t1": parseFloat(values[1]),
-                    "t2": parseFloat(values[2]),
-                    "t3": parseFloat(values[3]),
-                    "t4": parseFloat(values[4]),
-                    "t5": parseFloat(values[5]),
-                    "t6": parseFloat(values[6]),
-                    "t7": parseFloat(values[7]),
-                    "t8": parseFloat(values[8]),
-                    "t9": parseFloat(values[9]),
-                    "t10": parseFloat(values[10]),
-                    "t11": parseFloat(values[11]),
-                    "t12": parseFloat(values[12]),
-                    "t13": parseFloat(values[13]),
-                    "t14": parseFloat(values[14]),
-                    "t15": parseFloat(values[15]),
-                    "t16": parseFloat(values[16]),
+            }
+
+            request = request + "/" + this.output;
+            console.log(request);
+            // var urlEncoded = encodeURIComponent(request);
+            // console.log(urlEncoded);
+
+            const { data } = got.post(request, {
+                json: {
+                    hello: 'world'
                 }
-                node.send({ payload: output });
-            });
+            }).json();
+
+            console.log(data);
+
+            console.log("Input");
+            console.log(msg.payload);
+            node.send(msg);
         });
-
     }
-
-
-
-    RED.nodes.registerType("smartpi-e.digital-out", SmartPiEtemperatureValues);
+    RED.nodes.registerType("smartpi-e.digital-out", SmartPiDigitalOut);
 }

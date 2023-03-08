9913937261fa27f985976e60961eb7a67f3a70dd
Jens Ramhorst <j.ramhorst@enerserve.eu>
2023-03-08 11:24:40 +0100

    change version number to 0.2.4 modified:   package.json
    added option for trigger
    modified:   smartpi-input.html
    modified:   smartpi-input.js

--- smartpi-input.js
+++ smartpi-input.js
@@ -1,61 +1,80 @@
 module.exports = function(RED) {
+
+    function ReadValues(node, fs) {
+
+        var out = 0.0;
+
+        data = fs.readFileSync('/var/tmp/smartpi/smartpi_values', 'utf8');
+
+        var values = data.split(';');
+
+        switch (node.indicator) {
+            case "p":
+                out = parseFloat(values[8]) + parseFloat(values[9]) + parseFloat(values[10]);
+                break;
+            case "p1":
+                out = parseFloat(values[8]);
+                break;
+            case "p2":
+                out = parseFloat(values[9]);
+                break;
+            case "p3":
+                out = parseFloat(values[10]);
+                break;
+            case "i":
+                out = parseFloat(values[1]) + parseFloat(values[2]) + parseFloat(values[3]);
+                break;
+            case "i1":
+                out = parseFloat(values[1]);
+                break;
+            case "i2":
+                out = parseFloat(values[2]);
+                break;
+            case "i3":
+                out = parseFloat(values[3]);
+                break;
+            case "i4":
+                out = parseFloat(values[4]);
+                break;
+            case "v1":
+                out = parseFloat(values[5]);
+                break;
+            case "v2":
+                out = parseFloat(values[6]);
+                break;
+            case "v3":
+                out = parseFloat(values[7]);
+                break;
+        }
+        return out;
+    }
+
+
+
     function SmartPiInput(config) {
         RED.nodes.createNode(this, config);
 
         this.indicator = config.indicator;
+        this.usetrigger = config.usetrigger;
+
         var node = this;
 
- const fs = require('fs');
- const watch = require('node-watch');
-  watch( '/var/tmp/smartpi/smartpi_values', { recursive: true }, function(evt, name) {
-      fs.readFile('/var/tmp/smartpi/smartpi_values', 'utf8', function (err,data) {
-        if (err) {
-          return console.log(err);
+        const fs = require('fs');
+        const watch = require('node-watch');
+        var output = 0.0;
+        if (this.usetrigger == false) {
+            watch('/var/tmp/smartpi/smartpi_values', { recursive: true }, function(evt, name) {
+                output = ReadValues(node, fs);
+                node.send({ payload: output });
+            });
+        } else {
+            node.on('input', function(msg) {
+                if ((msg.payload == "1") || (msg.payload == true)) {
+                    output = ReadValues(node, fs);
+                    node.send({ payload: output });
+                }
+            });
         }
-        var values = data.split(';');
-				var output = 0.0
-				switch (node.indicator) {
-					case "p":
-						output = parseFloat(values[8]) + parseFloat(values[9]) + parseFloat(values[10]);
-						break;
-					case "p1":
-						output = parseFloat(values[8]);
-						break;
-					case "p2":
-						output = parseFloat(values[9]);
-						break;
-					case "p3":
-						output = parseFloat(values[10]);
-						break;
-					case "i":
-						output = parseFloat(values[1]) + parseFloat(values[2]) + parseFloat(values[3]);
-						break;
-					case "i1":
-						output = parseFloat(values[1]);
-						break;
-					case "i2":
-						output = parseFloat(values[2]);
-						break;
-					case "i3":
-						output = parseFloat(values[3]);
-						break;
-					case "i4":
-						output = parseFloat(values[4]);
-						break;
-					case "v1":
-						output = parseFloat(values[5]);
-						break;
-					case "v2":
-						output = parseFloat(values[6]);
-						break;
-					case "v3":
-						output = parseFloat(values[7]);
-						break;
-				}
-        node.send({payload:output});
-      });
-  });
-
     }
 
 
